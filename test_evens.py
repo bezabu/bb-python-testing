97f946a96d6a5c4b12724e4a9256410c4e0b277e
bezabu <>
2024-02-02 12:12:28 +0000

    refactor code

--- test_evens.py
+++ test_evens.py
@@ -3,9 +3,15 @@ from evens import even_number_of_evens
 
 
 class TestEvens(unittest.TestCase):
-    
-    def test_function_returns_True(self):
-        self.assertTrue(even_number_of_evens([]))
+    def test_throws_error_if_value_passed_is_not_list(self):
+        self.assertRaises(TypeError, even_number_of_evens, 4)
+
+    def test_values_in_list(self):
+        self.assertEqual(even_number_of_evens([]), False)
+        self.assertEqual(even_number_of_evens([2, 4]), True)
+        self.assertEqual(even_number_of_evens([2]), False)
+        self.assertEqual(even_number_of_evens([1, 3, 5]), False)
 
 
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
